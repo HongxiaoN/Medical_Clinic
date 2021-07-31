699564cf6669d51dde4f14bbfade7bc6484f6ca9
limlei2 <limlei2002@outlook.com>
2021-08-01 02:46:54 +0800

    appointmentactivity changes

--- app/src/main/java/com/example/medical_clinic/AppointmentActivity.java
+++ app/src/main/java/com/example/medical_clinic/AppointmentActivity.java
@@ -3,6 +3,7 @@ package com.example.medical_clinic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AppointmentActivity extends AppCompatActivity {
 
@@ -10,5 +11,117 @@ public class AppointmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
+
+        TextView apnt1col1 = findViewById(R.id.apnt1col1);
+        apnt1col1.setText("2/8/2021");
+        TextView apnt1col2 = findViewById(R.id.apnt1col2);
+        apnt1col2.setText("10:00");
+        TextView apnt1col3 = findViewById(R.id.apnt1col3);
+        apnt1col3.setText("11:30");
+        TextView apnt1col4 = findViewById(R.id.apnt1col4);
+        apnt1col4.setText("User 1");
+        TextView apnt1col5 = findViewById(R.id.apnt1col5);
+        apnt1col5.setText("Covid Test");
+        TextView apnt2col1 = findViewById(R.id.apnt2col1);
+        apnt2col1.setText("2/8/2021");
+        TextView apnt2col2 = findViewById(R.id.apnt2col2);
+        apnt2col2.setText("11:00");
+        TextView apnt2col3 = findViewById(R.id.apnt2col3);
+        apnt2col3.setText("12:00");
+        TextView apnt2col4 = findViewById(R.id.apnt2col4);
+        apnt2col4.setText("User 2");
+        TextView apnt2col5 = findViewById(R.id.apnt2col5);
+        apnt2col5.setText("Vaccine");
+        TextView apnt3col1 = findViewById(R.id.apnt3col1);
+        apnt3col1.setText("2/8/2021");
+        TextView apnt3col2 = findViewById(R.id.apnt3col2);
+        apnt3col2.setText("8:00");
+        TextView apnt3col3 = findViewById(R.id.apnt3col3);
+        apnt3col3.setText("11:00");
+        TextView apnt3col4 = findViewById(R.id.apnt3col4);
+        apnt3col4.setText("User 3");
+        TextView apnt3col5 = findViewById(R.id.apnt3col5);
+        apnt3col5.setText("Vaccine");
+        TextView apnt4col1 = findViewById(R.id.apnt4col1);
+        apnt4col1.setText("3/8/2021");
+        TextView apnt4col2 = findViewById(R.id.apnt4col2);
+        apnt4col2.setText("12:00");
+        TextView apnt4col3 = findViewById(R.id.apnt4col3);
+        apnt4col3.setText("13:00");
+        TextView apnt4col4 = findViewById(R.id.apnt4col4);
+        apnt4col4.setText("User 4");
+        TextView apnt4col5 = findViewById(R.id.apnt4col5);
+        apnt4col5.setText("Blood Test");
+        TextView apnt5col1 = findViewById(R.id.apnt5col1);
+        apnt5col1.setText("3/8/2021");
+        TextView apnt5col2 = findViewById(R.id.apnt5col2);
+        apnt5col2.setText("11:00");
+        TextView apnt5col3 = findViewById(R.id.apnt5col3);
+        apnt5col3.setText("13:00");
+        TextView apnt5col4 = findViewById(R.id.apnt5col4);
+        apnt5col4.setText("User 5");
+        TextView apnt5col5 = findViewById(R.id.apnt5col5);
+        apnt5col5.setText("Blood Test");
+        TextView apnt6col1 = findViewById(R.id.apnt6col1);
+        apnt6col1.setText("3/8/2021");
+        TextView apnt6col2 = findViewById(R.id.apnt6col2);
+        apnt6col2.setText("10:00");
+        TextView apnt6col3 = findViewById(R.id.apnt6col3);
+        apnt6col3.setText("12:30");
+        TextView apnt6col4 = findViewById(R.id.apnt6col4);
+        apnt6col4.setText("User 6");
+        TextView apnt6col5 = findViewById(R.id.apnt6col5);
+        apnt6col5.setText("Body Checkup");
+        TextView apnt7col1 = findViewById(R.id.apnt7col1);
+        apnt7col1.setText("4/8/2021");
+        TextView apnt7col2 = findViewById(R.id.apnt7col2);
+        apnt7col2.setText("10:00");
+        TextView apnt7col3 = findViewById(R.id.apnt7col3);
+        apnt7col3.setText("11:00");
+        TextView apnt7col4 = findViewById(R.id.apnt7col4);
+        apnt7col4.setText("User 7");
+        TextView apnt7col5 = findViewById(R.id.apnt7col5);
+        apnt7col5.setText("Cancer Treatment");
+        TextView apnt8col1 = findViewById(R.id.apnt8col1);
+        apnt8col1.setText("4/8/2021");
+        TextView apnt8col2 = findViewById(R.id.apnt8col2);
+        apnt8col2.setText("8:00");
+        TextView apnt8col3 = findViewById(R.id.apnt8col3);
+        apnt8col3.setText("9:00");
+        TextView apnt8col4 = findViewById(R.id.apnt8col4);
+        apnt8col4.setText("User 8");
+        TextView apnt8col5 = findViewById(R.id.apnt8col5);
+        apnt8col5.setText("Physical Therapy");
+        TextView apnt9col1 = findViewById(R.id.apnt9col1);
+        apnt9col1.setText("5/8/2021");
+        TextView apnt9col2 = findViewById(R.id.apnt9col2);
+        apnt9col2.setText("14:00");
+        TextView apnt9col3 = findViewById(R.id.apnt9col3);
+        apnt9col3.setText("15:00");
+        TextView apnt9col4 = findViewById(R.id.apnt9col4);
+        apnt9col4.setText("User 9");
+        TextView apnt9col5 = findViewById(R.id.apnt9col5);
+        apnt9col5.setText("Covid Test");
+        TextView apnt10col1 = findViewById(R.id.apnt10col1);
+        apnt10col1.setText("");
+        TextView apnt10col2 = findViewById(R.id.apnt10col2);
+        apnt10col2.setText("");
+        TextView apnt10col3 = findViewById(R.id.apnt10col3);
+        apnt10col3.setText("");
+        TextView apnt10col4 = findViewById(R.id.apnt10col4);
+        apnt10col4.setText("");
+        TextView apnt10col5 = findViewById(R.id.apnt10col5);
+        apnt10col5.setText("");
+
+
+
+
+
+
+
+
+
+
+
     }
 }
