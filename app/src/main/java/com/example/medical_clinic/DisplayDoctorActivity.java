c46fa65cf5bc83c3933200ccc781d95b9ea7d7c3
Kuma <1078341590@qq.com>
2021-08-01 21:20:17 +0800

    add the display doctor ("upcoming appointment" bottom)

--- app/src/main/java/com/example/medical_clinic/DisplayDoctorActivity.java
+++ app/src/main/java/com/example/medical_clinic/DisplayDoctorActivity.java
@@ -3,6 +3,7 @@ package com.example.medical_clinic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class DisplayDoctorActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class DisplayDoctorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_doctor);
+
+        CheckBox checkbox1 = findViewById(R.id.checkBox6);
+        checkbox1.setText("Jane Doe");
+
+        CheckBox checkbox2 = findViewById(R.id.checkBox4);
+        checkbox2.setText("Jane Doe1");
+
+        CheckBox checkbox3 = findViewById(R.id.checkBox7);
+        checkbox2.setText("Jane Doe2");
+
+        CheckBox checkbox4 = findViewById(R.id.checkBox8);
+        checkbox2.setText("wait for database");
+
+
+
     }
 }
