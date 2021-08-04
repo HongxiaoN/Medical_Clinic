758fe3fad32d1ecdf55531088a559f541477a9d7
Hongxiao <hongxiao.niu@mail.utoronto.ca>
2021-08-04 15:47:16 +0800

    Hongxiao link project to Firebase, testing version2

--- app/src/main/java/com/example/medical_clinic/MainActivity.java
+++ app/src/main/java/com/example/medical_clinic/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.medical_clinic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +26,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Write a message to the database
-//        FirebaseDatabase.getInstance().getReference("Test2").setValue("bbb");
+                FirebaseDatabase.getInstance().getReference("Test2").setValue("bbb")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Good", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Bad", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     public void displayDoctor(View view){
