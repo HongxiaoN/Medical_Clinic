20eb753375b1c8679a6181ca0e483cbb43999789
bias <marriewang221@gmail.com>
2021-08-05 00:27:44 -0400

    Mary Wang created doctor main page activity and view upcoming sppointments, added two buttons corresponding to doctor's upcoming appointment and schedule

---
+++ app/src/main/java/com/example/medical_clinic/ListOfPatientActivity.java
@@ -0,0 +1,17 @@
+package com.example.medical_clinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class ListOfPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_patient);
+
+    }
+}
---
+++ app/src/main/java/com/example/medical_clinic/doctor_mainpage.java
@@ -0,0 +1,21 @@
+package com.example.medical_clinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class doctor_mainpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_mainpage);
+    }
+
+    public void displayListOfPatients(View view){
+        Intent intent = new Intent(this, ListOfPatientActivity.class);
+        startActivity(intent);
+    }
+}
